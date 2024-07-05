80e3a349b861fe93bc54daa4c924ec5da1e28e39
woobyoungho <dragond68@d80.mail.kakaowork.com>
2024-07-05 09:47:38 +0900

    transfer update

--- autotrading/wbh_auto.py
+++ autotrading/wbh_auto.py
@@ -1,8 +1,14 @@
 import pyupbit
 import pybithumb
 import time
+from datetime import datetime
+import requests
+import jwt
+import uuid
+import hashlib
+from urllib.parse import urlencode
 
-with open("wbh_keys.txt") as f:
+with open("wbh_keys.txt", encoding='utf-8') as f:
     lines = f.readlines()
     bit_key = lines[0].strip()
     bit_secret = lines[1].strip()
@@ -11,6 +17,12 @@ with open("wbh_keys.txt") as f:
     up_key = lines[2].strip()
     up_secret = lines[3].strip()
     upbit = pyupbit.Upbit(up_key, up_secret)
+
+    upbit_wallet_address = lines[4].strip()
+    
+    bank_name = lines[5].strip()                # 예: 신한은행
+    bank_account = lines[6].strip()             # 예: 계좌번호
+    holder_name = lines[7].strip()              # 예: 예금주명
 
 def get_bithumb_orderbook():
     bithumb_orderbook = pybithumb.get_orderbook('USDT')             # 빗썸 오더북
@@ -42,7 +54,50 @@ def get_balances():
     bithumb_balance_krw = int(bithumb_balance[2])                   # 빗썸 원화 잔고
     upbit_balance_coin = int(upbit.get_balance('KRW-USDT'))         # 업빗 테더 잔고
     upbit_balance_krw = int(upbit.get_balance('KRW'))               # 업빗 원화 잔고
+
     return bithumb_balance_coin, bithumb_balance_krw, upbit_balance_coin, upbit_balance_krw
+
+##########################################################################################################
+###### 업비트 access token 생성
+def generate_upbit_token(access_key, secret_key):
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4())
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    return authorize_token
+
+###### 빗썸에서 업비트로 USDT 전송
+def transfer_bithumb_to_upbit(amount, address):
+    currency = "USDT"
+    destination = address  # 업비트의 USDT 지갑 주소
+    response = bithumb.bithumb_private_api('/trade/btc_withdrawal', {
+        'currency': currency,
+        'units': amount,
+        'address': destination,
+    })
+    return response
+
+###### 업비트에서 빗썸으로 원화 전송
+def transfer_upbit_to_bithumb(amount):
+    upbit_access_token = generate_upbit_token(up_key, up_secret)
+
+    url = "https://api.upbit.com/v1/withdraws/krw"
+    headers = {
+        "Authorization": upbit_access_token,
+        "Content-Type": "application/json"
+    }
+    data = {
+        "amount": amount,
+        "bank": bank_name,
+        "account": bank_account,
+        "holder_name": holder_name
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
+##########################################################################################################
 
 def trade(flag):
     bithumb_1st_bids_price, bithumb_1st_bids_quantity, bithumb_1st_asks_price, bithumb_1st_asks_quantity = get_bithumb_orderbook()
@@ -62,50 +117,50 @@ def trade(flag):
     print()
     print('업빗 원화 잔고 :', format(upbit_balance_krw, ','), '원,', format(upbit_balance_coin, ','), '개')
     print()
-    
+
     #################################################################################################
-    ##### 업빗 즉시매도 가격 - 빗썸 즉시매수 가격 ≥ 2원 이면 빗썸에서 매수, 업빗에서 매도 ################
+    ##### 업빗 즉시매도 가격 - 빗썸 즉시매수 가격 ≥ 2원 ===> 빗썸에서 매수, 업빗에서 매도 ################
     #################################################################################################
-    # 빗썸에서 매수하고 업빗에서 매도하는 로직
     if (upbit_1st_bids_price - bithumb_1st_asks_price) >= 2:
+        #빗썸, 업빗 원화 잔고에 해당하는 테더 수량
         bithumb_amount = (bithumb_balance_krw / bithumb_1st_asks_price) * 0.7
-        upbit_amount = upbit_balance_coin * 0.7                                          #업빗 원화 잔고에 해당하는 테더 수량
+        upbit_amount = upbit_balance_coin * 0.7
         amount = min(bithumb_amount, upbit_amount, upbit_1st_bids_size * 0.7, bithumb_1st_asks_quantity * 0.7)
+
         if amount > 0:
             flag = "Y"
             print('빗썸 - ', bithumb_1st_asks_price, '원, ', amount, '개 매수')
             print('업빗 - ', upbit_1st_bids_price, '원, ',  amount, '개 매도')
             try:
-                bithumb.buy_market_order('USDT', amount)       # 빗썸 시장가 매수
-                upbit.sell_market_order('KRW-USDT', amount)    # 업빗 시장가 매도
+                #빗썸 시장가 즉시매수, 업빗 시장가 즉시매도
+                bithumb.buy_market_order('USDT', amount)
+                upbit.sell_market_order('KRW-USDT', amount)
             except Exception as e:
                 print('거래 실패: ', e)
 
+            bithumb_to_upbit_transfer_response = transfer_bithumb_to_upbit(amount, upbit_wallet_address)
+            print(bithumb_to_upbit_transfer_response)
+
+            upbit_to_bithumb_transfer_response = transfer_upbit_to_bithumb(upbit_balance_krw)
+            print(upbit_to_bithumb_transfer_response)
+
     ################################################################################################################################
-    ##### 빗썸 즉시매수 가격 - 업빗 즉시매도 가격 ≥ 2원 이면 빗썸에서 매도, 업빗에서 매수(이런 경우는 희박하기 때문에 전송) ################
+    ##### 빗썸 즉시매수 가격 - 업빗 즉시매도 가격 ≥ 2원 ===> 빗썸에서 매도, 업빗에서 매수(이런 경우는 희박하기 때문에 전송) ################
     ################################################################################################################################
-    # 빗썸에서 매도하고 업비트에서 매수하는 로직
-    # if (bithumb_1st_asks_price - upbit_1st_bids_price) >= 2:
-    #     upbit_amount = upbit_balance_coin                                          #업빗 원화 잔고에 해당하는 테더 수량
-    #     amount = min(upbit_amount, bithumb_1st_asks_quantity * 0.7, upbit_1st_bids_size * 0.7)
-    #     if amount > 0:
-    #         print('빗썸 - ', bithumb_1st_asks_price, '원, ', amount, '개 매도')
-    #         print('업빗 - ', upbit_1st_bids_price, '원, ',  amount, '개 매수')
-            # try:
-            #     bithumb.sell_market_order('USDT', str(amount))      # 빗썸 시장가 매도
-            #     upbit.buy_market_order('KRW-USDT', amount)     # 업빗 시장가 매수
-            # except Exception as e:
-            #     print('거래 실패: ', e)
 
     return flag
-    
+
 while True:
     try:
+        now = datetime.now()
         flag = "N"
-        trade(flag)
-        time.sleep(0.15)  # 1초마다 반복
+
         if trade(flag) == "Y":
+            print("문자열 변환 : ", now.strftime('%Y-%m-%d %H:%M:%S'))
             exit()
+
+        time.sleep(0.15)  # 0.15초마다 반복
+        
     except Exception as e:
         print('Error: ', e)
         time.sleep(5)
